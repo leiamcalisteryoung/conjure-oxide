3aa11ef3b4e389e42add07435a618dd168e7ce6e
leiamcalisteryoung <l.mcalisteryoung@gmail.com>
2025-02-18 10:12:15 +0000

    Actions: Documentation for

--- docs/conjure_core/ast/sidebar-items.js
+++ docs/conjure_core/ast/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Atom","Domain","Expression","Literal","Name","Range"],"mod":["model","pretty","types"],"struct":["DecisionVariable","SymbolTable"]};
+window.SIDEBAR_ITEMS = {"enum":["Atom","DeclarationKind","Domain","Expression","Literal","Name","Range","ReturnType"],"mod":["pretty","serde"],"struct":["DecisionVariable","Declaration","Model","SerdeModel","SubModel","SymbolTable"],"trait":["Typeable"]};
