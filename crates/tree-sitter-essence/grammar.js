875c3cb04fab742a8eaefedd1b9af18eb7573432
leiamcalisteryoung <l.mcalisteryoung@gmail.com>
2025-02-27 16:05:23 +0000

    Small fixes (no change in functionality)

--- crates/tree-sitter-essence/grammar.js
+++ crates/tree-sitter-essence/grammar.js
@@ -75,36 +75,16 @@ module.exports = grammar({
       $.or_expr,
       $.implication,
       $.quantifier_expr,
-      $.constant,
-      $.variable,
       $.from_solution
     ),
 
-    not_expr: $ => prec(20, seq("!", $.expression)),
-
-    abs_value: $ => prec(20, seq("|", $.expression, "|")),
-
-    exponent: $ => prec(18, prec.right(seq($.expression, "**", $.expression))),
-
-    negative_expr: $ => prec(15, prec.left(seq("-", $.expression))),
-
-    product_expr: $ => prec(10, prec.left(seq($.expression, $.multiplicative_op, $.expression))),
-
-    multiplicative_op: $ => choice("*", "/", "%"),
-
-    sum_expr: $ => prec(1, prec.left(seq($.expression, $.additive_op, $.expression))),
-
-    additive_op: $ => choice("+", "-"),
-
-    comparison: $ => prec(0, prec.left(seq($.expression, $.comp_op, $.expression))),
-
-    comp_op: $ => choice("=", "!=", "<=", ">=", "<", ">"),
-
-    and_expr: $ => prec(-1, prec.left(seq($.expression, "/\\", $.expression))),
-
-    or_expr: $ => prec(-2, prec.left(seq($.expression, "\\/", $.expression))),
-
-    implication: $ => prec(-4, prec.left(seq($.expression, "->", $.expression))),
+    not_expr: $ => prec(20, seq("!", choice($.boolean_expr, $.comparison_expr, $.primary_expr))),
+    
+    and_expr: $ => prec(-1, prec.left(seq(choice($.boolean_expr, $.comparison_expr, $.primary_expr), "/\\", choice($.boolean_expr, $.comparison_expr, $.primary_expr)))),
+    
+    or_expr: $ => prec(-2, prec.left(seq(choice($.boolean_expr, $.comparison_expr, $.primary_expr), "\\/", choice($.boolean_expr, $.comparison_expr, $.primary_expr)))),
+    
+    implication: $ => prec(-4, prec.left(seq(choice($.boolean_expr, $.comparison_expr, $.primary_expr), "->", choice($.boolean_expr, $.comparison_expr, $.primary_expr)))),
 
     quantifier_expr: $ => prec(-10, seq(
       choice("and", "or", "min", "max", "sum", "allDiff"),
@@ -119,6 +99,45 @@ module.exports = grammar({
       $.variable,
       ")"
     ),
+
+    comparison_expr: $ => prec(0, prec.left(seq(
+      choice($.boolean_expr, $.arithmetic_expr), 
+      $.comparative_op,
+      choice($.boolean_expr, $.arithmetic_expr)
+    ))),
+
+    comparative_op: $ => choice("=", "!=", "<=", ">=", "<", ">"),
+
+    arithmetic_expr: $ => choice(
+      $.primary_expr,
+      $.negative_expr,
+      $.abs_value,
+      $.exponent,
+      $.product_expr,
+      $.sum_expr
+    ),
+
+    primary_expr: $ => choice(
+      $.sub_expr,
+      $.constant,
+      $.variable
+    ),
+
+    sub_expr: $ => seq("(", $.expression, ")"),
+
+    negative_expr: $ => prec(15, prec.left(seq("-", $.arithmetic_expr))),
+    
+    abs_value: $ => prec(20, seq("|", $.arithmetic_expr, "|")),
+    
+    exponent: $ => prec(18, prec.right(seq($.arithmetic_expr, "**", $.arithmetic_expr))),
+
+    product_expr: $ => prec(10, prec.left(seq($.arithmetic_expr, $.mulitcative_op, $.arithmetic_expr))),
+    
+    mulitcative_op: $ => choice("*", "/", "%"),
+    
+    sum_expr: $ => prec(1, prec.left(seq($.arithmetic_expr, $.additive_op, $.arithmetic_expr))),
+    
+    additive_op: $ => choice("+", "-"),
 
     dominance_relation: $ => seq(
       "dominanceRelation",
